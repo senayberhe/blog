19e70a3a7594c222768121851030a32454ae0961
Senay Berhe <tsionberhe@gmail.com>
2019-12-11 11:41:30 -0800

    added templates

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,13 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-	return "Hello, World!"
+    user = {'username': 'Senay'}
+    return '''
+<html>
+	<head>
+		<body>
+			<h1>Hello, ''' + user['username'] + '''!</h1>
+  		</body>
+  	</head>
+</html>'''
