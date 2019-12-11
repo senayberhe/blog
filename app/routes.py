6fce9bd6b55277a784cc7a88e4e63fb9e13a981b
Senay Berhe <tsionberhe@gmail.com>
2019-12-11 12:13:13 -0800

    added files

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Senay'}
-    return '''
-<html>
-	<head>
-		<body>
-			<h1>Hello, ''' + user['username'] + '''!</h1>
-  		</body>
-  	</head>
-</html>'''
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
