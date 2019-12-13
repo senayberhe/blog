89f32dd009498d648b3dde5b21b6f30e3d101696
Senay Berhe <tsionberhe@gmail.com>
2019-12-13 12:29:03 -0800

    mofidied packages

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['tsionberhe@gmail.com']
+    POSTS_PER_PAGE = 25
