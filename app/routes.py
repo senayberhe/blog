31a43287c9d665743fd9b288ecc7fc321fbe4f61
Senay Berhe <tsionberhe@gmail.com>
2019-12-13 14:19:26 -0800

    added files for email support

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 from app.forms import PostForm, ResetPasswordRequestForm
 from app.models import Post
-from app.email import send_password_reet_email
+from app.email import send_password_reset_email, ResetPasswordForm
 
 
 @app.before_request
@@ -170,3 +170,18 @@ def reset_password_request():
     return render_template('reset_password_request.html',
                            title='Reset Password', form=form)
 
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form=form)
